dcbcc6a03158a931c2b0fd9430cbf273928c5427
Marco Mele <melemarco94@gmail.com>
2017-10-11 17:52:29 -0500

    tweepy exploited

--- outcomes.py
+++ outcomes.py
@@ -7,6 +7,7 @@ try:
 	from urllib.parse import parse_qs
 except ImportError:
 	from urlparse import parse_qs
+import tweepy
 
 def getcredentials():
 	credentials = {
@@ -21,18 +22,27 @@ def getcredentials():
 	return credentials
 
 def authenticate(credentials):
-	oauth = OAuth(credentials["ACCESS_TOKEN"],
+	return OAuth1(credentials["ACCESS_TOKEN"],
 		credentials["ACCESS_SECRET"],
 		credentials["CONSUMER_KEY"],
 		credentials["CONSUMER_SECRET"])
-	return oauth
 
 if __name__ == '__main__':
+	# create authentication object
 	credentials = getcredentials()
-	oauth = authenticate(credentials)
-	urls = {
-		"authenticate_token" : "https://api.twitter.com/oauth2/token",
-		"request_token" : "https://api.twitter.com/oauth/request_token",
-		"authorize" : "https://api.twitter.com/oauth/authorize",
-		"access_token" : "https://api.twitter.com/oauth/access_token"
-	}
+	auth = tweepy.OAuthHandler(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
+	auth.set_access_token(credentials["ACCESS_TOKEN"], credentials["ACCESS_SECRET"])
+
+	api = tweepy.API(auth)
+	status = api.get_status(911994120658239488)
+	jstat = json.loads(json.dumps(status._json))
+	myId = jstat["user"]["id_str"]
+
+	user2 = api.get_user("Fra_Marcantoni")
+	juser2 = json.loads(json.dumps(user2._json))
+	fraId = juser2["id_str"]
+
+	timeline = api.user_timeline(fraId)
+	st = timeline[0]
+	jst = json.loads(json.dumps(st._json))
+	print jst["text"]
