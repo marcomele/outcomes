dadc3904cc14a91584ff3106450d3a56f50e73ab
Marco Mele <melemarco94@gmail.com>
2017-10-15 18:26:32 -0500

    merge previous ids per subcategory

---
+++ merge.py
@@ -0,0 +1,33 @@
+import os
+
+categories = {
+	"health" : {
+		"diseases" : 4,
+		"mental" : 45,
+		"pharmacy" : 3
+	},
+	"society" : {
+		"issues" : 1,
+		"law" : 9,
+		"relationships" : 2
+	},
+	"business" : {
+		"construction-and-maintenance" : 1,
+		"financial-services" : 4,
+		"investing" : 76
+	}
+}
+folder = "mergedData"
+os.system("rm -rf " + folder)
+os.system("mkdir " + folder)
+
+for category in categories:
+	for subcat in categories[category]:
+		with open(folder + "/" + category + "-" + subcat + ".txt", "w") as outFile:
+			print "merging files for " + category + "/" + subcat + "..."
+			for n in xrange(categories[category][subcat]):
+				filename = "previous-data/" + category + "/twitter_" + category + "_" + subcat + "_2014-03-01_2014-05-31_tweetids_" + str(n) + ".txt"
+				with open(filename, "r") as infile:
+					for line in infile:
+						outFile.write(line)
+print "done"
