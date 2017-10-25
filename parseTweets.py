aa3c1602d51f53daa9f9a378ba4a81dc9daeacc2
Marco Mele <melemarco94@gmail.com>
2017-10-24 21:23:10 -0500

    developed line splitting from output files

--- parseTweets.py
+++ parseTweets.py
@@ -12,63 +12,28 @@ def printKey(dictionary, key, depth = 0):
 		for subkey in dictionary[key]:
 			printKey(dictionary[key], subkey, depth + 1)
 
-# with open("data/sampleOutput") as sampleOutput:
-# 	for line in sampleOutput:
-# 		dictionary = ast.literal_eval(line)
-# 		for key in dictionary:
-# 			printKey(dictionary, key)
+def getOneTweet(line):
+	while True:
+		try:
+			[first, reminder] = line.split("}{", 1)
+			yield first + "}"
+			line = "{" + reminder
+		except ValueError:
+			yield line
+			break
 
-categories = {
-		"business" : {
-			"construction" : {
-				"count" : 0,
-				"unauthorized" : 0,
-				"status-not-found" : 0,
-				"internal-error" : 0,
-				"over-capacity" : 0,
-				"page-not-exists" : 0,
-				"user-suspended" : 0,
-				"connection-timed-out" : 0,
-				"total" : 0
-		}},
-		"society" : {
-			"issues" : {
-				"count" : 0,
-				"unauthorized" : 0,
-				"status-not-found" : 0,
-				"internal-error" : 0,
-				"over-capacity" : 0,
-				"page-not-exists" : 0,
-				"user-suspended" : 0,
-				"connection-timed-out" : 0,
-				"total" : 0
-		}}
-	}
+category = "society"
+subcategory = "issues"
 
-# parse nohup files and create metadata and dictionary
-for category in categories:
-	for subcategory in categories[category]:
-		nohupfile = "data/" + category + "-" + subcategory + "/nohup.out"
-		with open(nohupfile, "rU") as nohup:
-			for record in nohup:
-				if record and not "rate limit reached, waiting..." in record:
-					if "u'code': 179" in record:
-						categories[category][subcategory]["unauthorized"] += 1
-					elif "u'code': 144" in record:
-						categories[category][subcategory]["status-not-found"] += 1
-					elif "u'code' : 131" in record:
-						categories[category][subcategory]["internal-error"] +=1
-					elif "u'code': 130" in record:
-						categories[category][subcategory]["over-capacity"] += 1
-					elif "u'code': 34" in record:
-						categories[category][subcategory]["page-not-exists"] += 1
-					elif "u'code': 63" in record:
-						categories[category][subcategory]["user-suspended"] += 1
-					elif "api.twitter.com timed out" in record:
-						categories[category][subcategory]["connection-timed-out"] += 1
-					elif int(record):
-						categories[category][subcategory]["count"] += 1
-					categories[category][subcategory]["total"] -= 1
-			assert not sum(int(categories[category][subcategory][key]) for key in categories[category][subcategory])
-			categories[category][subcategory]["total"] *= -1
-pprint(categories)
+with open("data/sampleOutput") as sampleOutput:
+	for line in sampleOutput:
+		for literal in getOneTweet(line):
+			print literal
+
+		continue
+		try:
+			dictionary = ast.literal_eval(line)
+			for key in dictionary:
+				printKey(dictionary, key)
+		except SyntaxError:
+			print "bad formatted file --- all dictionaries must be newline separatad"
