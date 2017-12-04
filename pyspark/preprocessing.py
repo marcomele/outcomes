d6d5f4e08a099204833f3978d132e787be7a9ddc
Marco Mele <melemarco94@gmail.com>
2017-12-03 19:40:59 -0600

    data cleaning

--- pyspark/preprocessing.py
+++ pyspark/preprocessing.py
@@ -1,12 +1,27 @@
 import os
 import sys
 import json
+import re
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setAppName("edu.uic.cs.cs594.fakenews")
+def clean(tweet):
+    text = tweet["text"]
+    text = re.sub(r'http\S+', '', text)
+    text = re.sub(r'\&\S+;', '', text)
+    text.replace('#', '')
+    tweet["text"] = text
+    return tweet
+
+conf = SparkConf().setAppName("edu.uic.cs.cs594.fakenews.preprocessing.py")
 sc = SparkContext(conf = conf)
 
+# load tweets
 input_rdd = sc.textFile(sys.argv[1])
-json_rdd = input_rdd.map(lambda string: json.loads(string)
-authors_rdd = json_rdd.map(lambda j: j["author"]).distinct()
-authors_rdd.saveAsTextFile(sys.argv[0] + "_out")
+structured_rdd = input_rdd.map(lambda string: json.loads(string))
+
+# data cleaning
+cleaned_rdd = structured_rdd.map(lambda tweet: clean(tweet))
+
+# output
+output_rdd = cleaned_rdd.map(lambda tweet: json.dumps(tweet))
+output_rdd.saveAsTextFile(sys.argv[1].split("/")[0] + "/output")
